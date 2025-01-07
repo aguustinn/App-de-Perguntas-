7c4cbdd9aeec3fdad6199178e752a0f2d54898f5
agustinn-dev <agustinpenelas@gmail.com>
2025-01-06 22:05:59 -0300

    Adicionei a primeira pergunta e criei botões de resposta

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,36 @@ main() {
 }
 
 class PerguntaApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
     return MaterialApp(
-      home: Text("Perguntas com Flutter!!!"),
-      darkTheme: ThemeData(brightness: Brightness.light),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: null,
+            ),
+            ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
