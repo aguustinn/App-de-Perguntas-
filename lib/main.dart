0d40a5a126ceeec085ea769f528179395e593580
agustinn-dev <agustinpenelas@gmail.com>
2025-01-07 23:09:37 -0300

    Criando o Widget Resposta

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
+import './resposta.dart';
 
 main() {
   runApp(PerguntaApp());
@@ -31,18 +32,9 @@ class _PerguntaAppState extends State<PerguntaApp> {
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]),
-            ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: _responder,
-            ),
-            ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: _responder,
-            ),
+            Resposta('Reposta 1'),
+            Resposta('Reposta 2'),
+            Resposta('Reposta 3'),
           ],
         ),
       ),
